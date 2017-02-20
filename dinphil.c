9c864f8f3914f698facfef59ba8184333d4a0f81
Harijith P <harijith.p@kau.in>
2017-02-20 19:05:03 +0530

    Update dinphil.c

--- dinphil.c
+++ dinphil.c
@@ -2,12 +2,12 @@
 #include <semaphore.h>
 #include <pthread.h>
   
-#define N 5
-#define THINKING 0
-#define HUNGRY 1
-#define EATING 2
-#define LEFT (ph_num+4)%N
-#define RIGHT (ph_num+1)%N
+#define N 			5
+#define THINKING 	0
+#define HUNGRY 		1
+#define EATING 		2
+#define LEFT 		(ph_num + 4) % N
+#define RIGHT 		(ph_num + 1) % N
   
 sem_t mutex;
 sem_t S[N];
@@ -24,16 +24,18 @@ int main()
 {
     int i;
     pthread_t thread_id[N];
+	
     sem_init(&mutex,0,1);
-    for(i=0;i<N;i++)
-        sem_init(&S[i],0,0);
-    for(i=0;i<N;i++)
+	
+    for(i = 0; i < N; i++)
+        sem_init(&S[i], 0, 0);
+    for(i = 0; i < N; i++)
     {
-        pthread_create(&thread_id[i],NULL,philospher,&phil_num[i]);
-        printf("Philosopher %d is thinking\n",i+1);
+        pthread_create(&thread_id[i], NULL, philospher, &phil_num[i]);
+        printf("Philosopher %d is thinking\n", i + 1);
     }
-    for(i=0;i<N;i++)
-        pthread_join(thread_id[i],NULL);
+    for(i = 0; i < N; i++)
+        pthread_join(thread_id[i], NULL);
 }
   
 void *philospher(void *num)
@@ -52,7 +54,7 @@ void take_fork(int ph_num)
 {
     sem_wait(&mutex);
     state[ph_num] = HUNGRY;
-    printf("Philosopher %d is Hungry\n",ph_num+1);
+    printf("Philosopher %d is Hungry\n", ph_num+1);
     test(ph_num);
     sem_post(&mutex);
     sem_wait(&S[ph_num]);
@@ -65,8 +67,8 @@ void test(int ph_num)
     {
         state[ph_num] = EATING;
         sleep(2);
-        printf("Philosopher %d takes fork %d and %d\n",ph_num+1,LEFT+1,ph_num+1);
-        printf("Philosopher %d is Eating\n",ph_num+1);
+        printf("Philosopher %d takes fork %d and %d\n", ph_num + 1, LEFT + 1, ph_num + 1);
+        printf("Philosopher %d is Eating\n", ph_num + 1);
         sem_post(&S[ph_num]);
     }
 }
@@ -75,8 +77,8 @@ void put_fork(int ph_num)
 {
     sem_wait(&mutex);
     state[ph_num] = THINKING;
-    printf("Philosopher %d putting fork %d and %d down\n",ph_num+1,LEFT+1,ph_num+1);
-    printf("Philosopher %d is thinking\n",ph_num+1);
+    printf("Philosopher %d putting fork %d and %d down\n", ph_num + 1, LEFT + 1, ph_num + 1);
+    printf("Philosopher %d is thinking\n", ph_num + 1);
     test(LEFT);
     test(RIGHT);
     sem_post(&mutex);
