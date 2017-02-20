8bb379520f5c14e56ff4f4e205338ab688b8e3ca
Harijith P <harijith.p@kau.in>
2017-02-20 19:00:54 +0530

    Update dinphil.c

--- dinphil.c
+++ dinphil.c
@@ -1,17 +1,6 @@
-/***********************************************************
-* You can use all the programs on  www.c-program-example.com
-* for personal and learning purposes. For permissions to use the
-* programs for commercial purposes,
-* contact info@c-program-example.com
-* To find more C programs, do visit www.c-program-example.com
-* and browse!
-* 
-*                      Happy Coding
-***********************************************************/
- 
-#include<stdio.h>
-#include<semaphore.h>
-#include<pthread.h>
+#include <stdio.h>
+#include <semaphore.h>
+#include <pthread.h>
   
 #define N 5
 #define THINKING 0
